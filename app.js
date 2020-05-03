58227864a33c7e7499c4e3d9384281c7e3fe6629
marccdb <marcio.bianco@gmail.com>
2020-05-03 15:02:35 -0300

    fist commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.listen(3000, () => console.log("listening on port 3000"));
+
+app.get("/", function (req, res){  
+
+
+    
+});
